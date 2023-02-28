60e98a9994194621ae05acbc4301741123863d53
Enyu Zhao <469070269@qq.com>
2023-02-28 12:49:34 -0800

    Q4 basic

--- homework2/Q4.sql
+++ homework2/Q4.sql
@@ -1,1 +1,43 @@
 -- I am using bit.io for this question
+
+create table video(
+    video_url varchar not null unique,
+    video_title varchar not null,
+    thumbnail int default 0,
+    description varchar,
+    duration int not null default 0,
+    category varchar,
+    --duration here is measured by how many seconds
+    primary key (video_url)
+
+
+
+
+);
+
+create table info_video(
+        video_url varchar not null,
+        keyword varchar not null,
+        primary key (video_url,keyword)
+)
+
+create table comment (
+    video_url varchar not null,
+    comment_id varchar not null,
+    commenter_id varchar not null,
+    comment_text varchar,
+    likes int default 0,
+    sentiment int default 0,
+)
+
+create table account(
+    owner_user_id varchar not null unique,
+    owner_name varchar,
+    email varchar,
+    user_address varchar,
+    age int default 0,
+    is_vid_creator bool default False,
+
+    primary key (owner_user_id)
+
+);
